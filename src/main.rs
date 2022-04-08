2e97195f519a664f422d5b976093cf5023e2dc4e
David Pacheco <dap@oxidecomputer.com>
2022-04-07 19:04:53 -0700

    an even simpler case reproduces it

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,14 @@
+use std::io::Write;
+
 fn main() {
     doit();
 }
 
 fn doit() {
-    let t = term::stdout();
-    if let Some(mut term) = t {
-        eprintln!("println to stderr (stdout found)");
-        println!("println to stdout (before)");
-        term.fg(term::color::GREEN).unwrap();
-        write!(term, "terminal write to stdout\n").unwrap();
-        term.reset().unwrap();
-        term.flush().unwrap();
-        println!("println to stdout (after)");
-    } else {
-        eprintln!("println to stderr (stdout not found)");
-    }
+    eprintln!("println to stderr (stdout found)");
+    println!("println to stdout (before)");
+    write!(std::io::stdout(), "write to stdout\n").unwrap();
+    println!("println to stdout (after)");
 }
 
 #[cfg(test)]
